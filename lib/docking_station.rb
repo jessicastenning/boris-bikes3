991f3775a7a466b0463618fb39cc4a03eeb86eaa
jessica stenning <jessicastenning@hotmail.co.uk>
2018-05-26 11:33:45 +0100

    adds dock bike functionality and returns docked bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,8 +2,14 @@ require_relative 'bike'
 
 class Dockingstation
 
+  attr_reader :bike
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
+    @bike = bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,23 @@
 require 'docking_station'
 
 describe Dockingstation do
-  it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to (:release_bike) }
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+  it { is_expected.to respond_to(:bike) }
 
   it 'releases a working bike' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike).to eq bike
   end
 end
